a9c5d830e28255f6923765ddd033d003ea83b0a4
username <dhavalchaudhari39@gmail.com>
2023-12-07 18:57:55 +0530

    added mui

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -4,7 +4,15 @@ import axios from 'axios'
 export default function SignupPage() {
   return (
     <div>
-      <Signup />
+      <Signup
+        onClick={async (username, password) => {
+          const response = await axios.post('/api/signup', {
+            username,
+            password,
+          })
+          localStorage.setItem('token', response.data.token)
+        }}
+      />
     </div>
   )
 }
